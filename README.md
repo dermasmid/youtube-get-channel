bcb9aa655039e9c37babb1b33918e688c71b744f
cheskel <twerskycheskel@gmail.com>
2021-06-06 18:53:36 +0300

    fix real headless

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Credit
 https://github.com/Shail-Shouryya/yt-videos-list
+
+# Build
+`python3 setup.py bdist_wheel`
--- get_channel/logic.py
+++ get_channel/logic.py
@@ -47,7 +47,14 @@ def execute(url, file_name, reverse_chronological, headless, scroll_pause_time, 
     def configure_chromedriver():
         options = webdriver.ChromeOptions()
         if headless is True:
-            options.add_argument('headless')
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-setuid-sandbox")
+            options.add_argument("--disable-dev-shm-using")
+            options.add_argument("--disable-extensions")
+            options.add_argument("--disable-gpu")
+            options.add_argument("start-maximized")
+            options.add_argument("disable-infobars")
+            options.add_argument("--headless")
         return webdriver.Chrome(chrome_options=options)
 
     def configure_bravedriver():
